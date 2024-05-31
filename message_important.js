fcde4f96162d55f4cc9b5c396ced0a61e3e10b03
Speiner <166366599+Speiner@users.noreply.github.com>
2024-05-31 09:28:43 +0200

    Update message_important.js

--- message_important.js
+++ message_important.js
@@ -1,18 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     const infoTextarea = document.getElementById('info');
     const saveMessageBtn = document.getElementById('saveMessageBtn');
+    const viewMessagesBtn = document.getElementById('viewMessagesBtn');
     const returnBtn = document.getElementById('returnBtn');
 
     // Sauvegarder le message important
     saveMessageBtn.addEventListener('click', function() {
-        const message = infoTextarea.value;
+        let message = infoTextarea.value;
+        message = message.replace(/\n/g, '<br>');
         localStorage.setItem('importantMessage', message);
     });
 
     // Charger le message important au chargement de la page
     function loadMessage() {
-        const storedMessage = localStorage.getItem('importantMessage');
+        let storedMessage = localStorage.getItem('importantMessage');
         if (storedMessage) {
+            storedMessage = storedMessage.replace(/<br>/g, '\n');
             infoTextarea.value = storedMessage;
         }
     }
@@ -22,5 +25,10 @@ document.addEventListener('DOMContentLoaded', function() {
     // Rediriger vers modification.html au clic sur le bouton Retour
     returnBtn.addEventListener('click', function() {
         window.location.href = 'modification.html';
+    });
+
+    // Rediriger vers affichaemes.html au clic sur le bouton Voir les messages importants
+    viewMessagesBtn.addEventListener('click', function() {
+        window.location.href = 'affichagemes.html';
     });
 });
