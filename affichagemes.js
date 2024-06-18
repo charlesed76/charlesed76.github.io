d28910e49780a32bb849d38df54ba66daf37a383
Speiner <166366599+Speiner@users.noreply.github.com>
2024-06-18 15:41:00 +0200

    Update affichagemes.js

--- affichagemes.js
+++ affichagemes.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const message = localStorage.getItem('importantMessage');
-    document.getElementById('displayMessage').innerHTML = message || 'Aucun message enregistré';
+    const message = localStorage.getItem('lastImportantMessage');
+
+    if (message) {
+        document.getElementById('displayMessage').innerHTML = message.replace(/<br>/g, '\n').split('\n').join('<br>');
+    } else {
+        document.getElementById('displayMessage').innerHTML = 'Aucun message enregistré';
+    }
 
     document.getElementById('returnBtn').addEventListener('click', function() {
         window.location.href = 'message_imporant.html';
